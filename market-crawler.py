c6b9165fc2b3f0dad9ad5bedfb6dcaec3e31425a
soulmachine <soulmachine@gmail.com>
2021-09-07 23:36:19 -0700

    Wrapped whole code with try except

--- market-crawler.py
+++ market-crawler.py
@@ -91,23 +91,28 @@ if __name__ == "__main__":
     for exchange in EXCHANGE_MARKET_TYPES:
         for market_type in sorted(EXCHANGE_MARKET_TYPES[exchange]):
             logging.info(f'{exchange} {market_type}')
-            json_obj = fetch_markets(exchange, market_type)
-            json_text = json.dumps(json_obj, indent=4, sort_keys=True) + '\n'
-            today = datetime.utcnow().isoformat()[:10]
-            latest_file = f'./data/{exchange}.{market_type}.json'
-            daily_file = f'./data/{exchange}.{market_type}.{today}.json'
+            try:
+                json_obj = fetch_markets(exchange, market_type)
+                json_text = json.dumps(
+                    json_obj, indent=4, sort_keys=True) + '\n'
+                today = datetime.utcnow().isoformat()[:10]
+                latest_file = f'./data/{exchange}.{market_type}.json'
+                daily_file = f'./data/{exchange}.{market_type}.{today}.json'
 
-            if os.path.exists(latest_file):
-                with open(latest_file, mode='rt') as f_in:
-                    json_text_old = f_in.read()
-            else:
-                json_text_old = ''
-            if json_text != json_text_old:
-                with open(daily_file, mode='wt') as f_out:
-                    f_out.write(json_text)
-                os.chdir('./data')
-                if json_text_old:
-                    os.unlink(os.path.basename(latest_file))
-                os.symlink(os.path.basename(daily_file),
-                           os.path.basename(latest_file))
-                os.chdir('..')
+                if os.path.exists(latest_file):
+                    with open(latest_file, mode='rt') as f_in:
+                        json_text_old = f_in.read()
+                else:
+                    json_text_old = ''
+                if json_text != json_text_old:
+                    with open(daily_file, mode='wt') as f_out:
+                        f_out.write(json_text)
+                    os.chdir('./data')
+                    if json_text_old:
+                        os.unlink(os.path.basename(latest_file))
+                    os.symlink(os.path.basename(daily_file),
+                               os.path.basename(latest_file))
+                    os.chdir('..')
+            except Exception as e:
+                logging.exception(
+                    f'{exchange} {market_type} fetch_markets failed')
